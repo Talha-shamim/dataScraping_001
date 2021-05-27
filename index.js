32a7a9f42a15d92162dd242fa91e8e0be1e4cc81
Talha-shamim <talhashamim001@gmail.com>
2021-05-28 00:11:51 +0530

    changed import to const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const fs = require("fs");
 const cors = require("cors");
-const dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = express();
