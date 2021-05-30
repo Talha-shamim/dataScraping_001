53244edabd63c89d87b9261cace3ec0602bf2576
Talha-shamim <talhashamim001@gmail.com>
2021-05-30 22:25:40 +0530

    gujarat

--- jsonFiles/Coordinates/MergeCoordinates/Gujratfinal.js
+++ jsonFiles/Coordinates/MergeCoordinates/Gujratfinal.js
@@ -1,53 +1,56 @@
 const googleGujrat = require("../ahmedabadcoordinates.json");
-const Gujrat = require("../../gujarat.json");
+const Gujrat = require("../../../jsonFiles/Ahmedabad.json");
 const fs = require("fs");
 
 function get() {
   Gujrat.map((dt) => {
     googleGujrat.map((gdt) => {
       if (dt.googleSearch === gdt.url) {
-        if(gdt.location){
-      dt.hospitalAddress = gdt.location;
+        if (gdt.location) {
+          dt.hospitalAddress = gdt.location;
         }
 
-        if(gdt.phone){
-      dt.phoneNo = gdt.phone;
-        }
-        
-        if(gdt.cordlat){
-      dt.latitude = gdt.cordlat;
+        if (gdt.phone) {
+          dt.phoneNo = gdt.phone;
         }
 
-        if(gdt.cordlon){
-      dt.longitude = gdt.cordlon;
+        if (gdt.cordlat) {
+          dt.latitude = gdt.cordlat;
         }
 
-
-    }
+        if (gdt.cordlon) {
+          dt.longitude = gdt.cordlon;
+        }
+      }
     });
   });
 
-  Gujrat.map(dt=> {
-
-    if(dt.phoneNo.length<=4){
-      dt.phoneNo="Not Available"
-    }
-    
-    if(dt.hospitalAddress==="Not Available" && dt.phoneNo==="Not Available"){
-      dt.rank=0
-    }
-    if(dt.hospitalAddress!=="Not Available" || dt.phoneNo!=="Not Available"){
-      dt.rank=1
-  }
-    if(dt.hospitalAddress!=="Not Available" && dt.phoneNo!=="Not Available"){
-      dt.rank=2
+  Gujrat.map((dt) => {
+    if (dt.phoneNo.length <= 4) {
+      dt.phoneNo = "Not Available";
     }
 
-
-    
-  })
+    if (
+      dt.hospitalAddress === "Not Available" &&
+      dt.phoneNo === "Not Available"
+    ) {
+      dt.rank = 0;
+    }
+    if (
+      dt.hospitalAddress !== "Not Available" ||
+      dt.phoneNo !== "Not Available"
+    ) {
+      dt.rank = 1;
+    }
+    if (
+      dt.hospitalAddress !== "Not Available" &&
+      dt.phoneNo !== "Not Available"
+    ) {
+      dt.rank = 2;
+    }
+  });
   fs.writeFile(
-    `jsonFiles/gujrat.json`,
+    `jsonFiles/gujarat.json`,
     JSON.stringify(Gujrat, null, 2),
     (error) => {
       if (error) {
--- jsonFiles/Coordinates/MergeCoordinates/haryanafinal.js
+++ jsonFiles/Coordinates/MergeCoordinates/haryanafinal.js
@@ -6,48 +6,49 @@ function get() {
   Gujrat.map((dt) => {
     googleGujrat.map((gdt) => {
       if (dt.googleSearch === gdt.url) {
-          if(gdt.location){
-        dt.hospitalAddress = gdt.location;
-          }
+        if (gdt.location) {
+          dt.hospitalAddress = gdt.location;
+        }
 
-          if(gdt.phone){
-        dt.phoneNo = gdt.phone;
-          }
-          
-          if(gdt.cordlat){
-        dt.latitude = gdt.cordlat;
-          }
+        if (gdt.phone) {
+          dt.phoneNo = gdt.phone;
+        }
 
-          if(gdt.cordlon){
-        dt.longitude = gdt.cordlon;
-          }
+        if (gdt.cordlat) {
+          dt.latitude = gdt.cordlat;
+        }
 
-
+        if (gdt.cordlon) {
+          dt.longitude = gdt.cordlon;
+        }
       }
-
- 
     });
   });
 
-  Gujrat.map(dt=> {
-
-    if(dt.phoneNo.length<=4){
-      dt.phoneNo="Not Available"
-    }
-    
-    if(dt.hospitalAddress==="Not Available" && dt.phoneNo==="Not Available"){
-      dt.rank=0
-    }
-    if(dt.hospitalAddress!=="Not Available" || dt.phoneNo!=="Not Available"){
-      dt.rank=1
-  }
-    if(dt.hospitalAddress!=="Not Available" && dt.phoneNo!=="Not Available"){
-      dt.rank=2
+  Gujrat.map((dt) => {
+    if (dt.phoneNo.length <= 4) {
+      dt.phoneNo = "Not Available";
     }
 
-
-    
-  })
+    if (
+      dt.hospitalAddress === "Not Available" &&
+      dt.phoneNo === "Not Available"
+    ) {
+      dt.rank = 0;
+    }
+    if (
+      dt.hospitalAddress !== "Not Available" ||
+      dt.phoneNo !== "Not Available"
+    ) {
+      dt.rank = 1;
+    }
+    if (
+      dt.hospitalAddress !== "Not Available" &&
+      dt.phoneNo !== "Not Available"
+    ) {
+      dt.rank = 2;
+    }
+  });
 
   fs.writeFile(
     `jsonFiles/haryana.json`,
